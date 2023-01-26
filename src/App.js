e2b47206406bbe9e5e279851d8b0521d7217ff33
Elaine <sosohn@gmail.com>
2023-01-26 14:56:42 -0800

    finish wave01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from '../src/components/ChatEntry'
+
+const MESSAGES = chatMessages;
+const message = MESSAGES[0]
 
 const App = () => {
   return (
@@ -9,8 +13,16 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/}
+        <ChatEntry
+        key={message.id}
+        id={message.id}       
+        sender={message.sender}
+        body={message.body}
+        timeStamp={message.timeStamp}
+        liked={message.liked}
+      />
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
