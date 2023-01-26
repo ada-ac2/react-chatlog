0a9a268daf693562ec9278742976cafabfede23f
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-01-26 14:18:30 -0800

    implemented wave 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+
+const chatMessage = {
+  id: 1,
+  sender: 'Vladimir',
+  body: 'why are you arguing with me',
+  timeStamp: '2018-05-29T22:49:06+00:00',
+  liked: false,
+};
 
 const App = () => {
   return (
@@ -9,6 +18,13 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry
+          id={chatMessage.id}
+          sender={chatMessage.sender}
+          body={chatMessage.body}
+          timeStamp={chatMessage.timeStamp}
+          liked={chatMessage.body}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
