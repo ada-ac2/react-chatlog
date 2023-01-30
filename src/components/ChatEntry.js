473188685c1389f4cad6613759a38510fe8bd24e
Yufei Bao <ybao1005@gmail.com>
2023-01-29 21:05:15 -0800

    wave 3 in progress

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const [isClicked, setIsClicked] = useState(false); 
+  const [clickCount, setClickCount] = useState(0)
+
+  const updateClickState = () => {
+    setIsClicked(!isClicked)
+  }
+  const updateClickCount = () => {
+    console.log('update click coount')
+    setClickCount(clickCount+1)
+  }
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
         <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
-        <button className="like">🤍</button>
+        <button className = "like" onClick={updateClickState}>{isClicked ? '❤️' : '🤍'}</button>
       </section>
     </div>
   )};
