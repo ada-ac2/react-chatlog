716a8eba6166dfa3f1931a6fe3e7593c7ed9859d
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-31 13:40:17 -0800

    Wave 01 - Rendered one ChatEntry component in App.js. Passing all wave01 tests.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const [chatData, updateChatData] = useState(chatMessages);
+  const likesChanged = (id) => {
+    const chats = chatData.map((chat) => {
+      if (chat.id === id) {
+        chat.liked = !chat.liked;
+      }
+      return chat;
+    });
+    updateChatData(chats);
+  };
   return (
     <div id="App">
-      <header>
-        <h1>Application title</h1>
+      <header id="App header">
+        <h1 id="App h1">Chat between</h1>
       </header>
-      <main>
+      <main id="App main">
+        <div>
+          {
+            <ChatEntry
+              id={chatData[0].id}
+              sender={chatData[0].sender}
+              body={chatData[0].body}
+              timeStamp={chatData[0].timeStamp}
+              liked={chatData[0].liked}
+            />
+          }
+        </div>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
