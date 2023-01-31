af28e8ec793a628528ae7921e2658c514cb12781
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-31 13:38:21 -0800

    Wave 01 - added ChatEntry proptypes and used them in JSX.used TimeStamp.js for chat time.

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({ id, sender, body, timeStamp, liked }) => {
+  const chatTime = <TimeStamp time={timeStamp} />;
+
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+    <div key={id} className="chat-entry local">
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+        <p>{body}</p>
+        <p className="entry-time">{chatTime}</p>
+        <button className="like">{liked}🤍</button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
