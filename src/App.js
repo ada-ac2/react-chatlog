a722cbbed648fd193f7e28ce15ebba74e0a18337
mckay <mckaybwarren@gmail.com>
2023-01-31 20:29:22 -0500

    add heart count display

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import chatMessages from './data/messages.json';
 const App = () => {
   const [entries, setEntries] = useState(chatMessages);
 
+  const getTotalHearts = () => {
+    let totalHearts = 0;
+    entries.forEach((entry) => {
+      if (entry.liked) {
+        totalHearts++;
+      }
+    });
+    return totalHearts;
+  };
+
   const updateEntry = (entryToUpdate) => {
     const updatedEntries = entries.map((entry) => {
       if (entry.id === entryToUpdate.id) {
@@ -20,6 +30,11 @@ const App = () => {
     <div id="App">
       <header>
         <h1>Application title</h1>
+        <section>
+          <h2 id="heartWidget" className="widget">
+            {getTotalHearts()} ❤️s
+          </h2>
+        </section>
       </header>
       <main>
         <ChatLog entries={entries} onUpdateLiked={updateEntry} />
