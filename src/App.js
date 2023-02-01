91fd0b504ddc990f8572ee15cd2815824692ef2f
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-31 23:02:47 -0800

    Wave03 - implemented totalLikes function to display number of hearts on top.

--- src/App.js
+++ src/App.js
@@ -14,10 +14,26 @@ const App = () => {
     });
     updateChatData(chats);
   };
+  const totalLikes = () => {
+    let total = 0;
+    for (let chat of chatData) {
+      if (chat.liked === true) {
+        total += 1;
+      }
+    }
+    return total;
+  };
   return (
     <div id="App">
       <header id="App header">
-        <h1 id="App h1">Chat between</h1>
+        <h1 id="App h1">
+          Chat between {chatData[0].sender} and {chatData[1].sender}
+        </h1>
+        <section id="App header section">
+          <h2 id="heartWidget" className="widget">
+            <span id="App span">{totalLikes()}❤️s</span>
+          </h2>
+        </section>
       </header>
       <main id="App main">
         <div>{<ChatLog entries={chatData} onUpdateLikes={updateLikes} />}</div>
