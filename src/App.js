71918ce44c43ed8cfe3911f0e262fbe8472472f4
diana <arreoladiana11@gmail.com>
2023-01-31 21:55:07 -0800

    wave1: pass props to ChatEntry component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry></ChatEntry>
       </main>
     </div>
   );
