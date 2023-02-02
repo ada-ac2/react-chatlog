c8e25b50948458e15bc0a01bf95a46a2d2cde467
nadxelle <nadxellehernandez@gmail.com>
2023-02-02 10:19:16 -0800

    added emoji to buttons to look like the picture provided

--- src/components/ColorChoice.js
+++ src/components/ColorChoice.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './ColorChoice.css';
 
 const ColorChoice = ({ senderName, newColor, updateColor }) => {
   return (
     <section className="widget">
       <h3 className={newColor}>{senderName}'s color:</h3>
-      <button onClick={() => updateColor('red')} className="red"></button>
-      <button onClick={() => updateColor('orange')} className="orange"></button>
-      <button onClick={() => updateColor('yellow')} className="yellow"></button>
-      <button onClick={() => updateColor('green')} className="green"></button>
-      <button onClick={() => updateColor('blue')} className="blue"></button>
-      <button onClick={() => updateColor('purple')} className="purple"></button>
+      <button onClick={() => updateColor('red')} className="red">
+        🔴
+      </button>
+      <button onClick={() => updateColor('orange')} className="orange">
+        🟠
+      </button>
+      <button onClick={() => updateColor('yellow')} className="yellow">
+        🟡
+      </button>
+      <button onClick={() => updateColor('green')} className="green">
+        🟢
+      </button>
+      <button onClick={() => updateColor('blue')} className="blue">
+        🔵
+      </button>
+      <button onClick={() => updateColor('purple')} className="purple">
+        🟣
+      </button>
     </section>
   );
 };
