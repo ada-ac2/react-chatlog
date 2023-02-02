1fa58cbeb08e4da98c9b059b87ee880797ac3517
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-02-02 02:57:43 -0700

    completed wave 01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+
+const message = {
+  id: 1,
+  sender: 'Vladimir',
+  body: 'why are you arguing with me',
+  timeStamp: '2018-05-29T22:49:06+00:00',
+  liked: false,
+};
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>OcelotChat</h1>
       </header>
       <main>
+        <ChatEntry
+          id={message.id}
+          sender={message.sender}
+          body={message.body}
+          timeStamp={message.timeStamp}
+          liked={message.liked}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({ sender, body, timeStamp }) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{body}</p>
+        <p className="entry-time">
+          <TimeStamp time={timeStamp} />
+        </p>
         <button className="like">🤍</button>
       </section>
     </div>
   );
 };
 
-ChatEntry.propTypes = {
-  //Fill with correct proptypes
-};
+ChatEntry.propTypes = {};
 
 export default ChatEntry;
