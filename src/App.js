fea50b09732f23cd7ea0b742cd0e966638173bbe
diana <arreoladiana11@gmail.com>
2023-02-01 18:10:29 -0800

    Set and update state for a liked message

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React from 'react';
 import './App.css';
 import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
+import { useState } from 'react';
 
 const App = () => {
+  const [entryData, setEntryData] = useState(chatMessages);
+
+  const updateLikedMessage = (id) => {
+    const allEntries = entryData.map(entry => {
+      if (entry.id === id) {
+        return {...entry, likedCount: entry.likedCount + 1};
+      } else {
+        return entry;
+      }
+    });
+
+    setEntryData(allEntries);
+  };
+
+  // const totalLikes = (entryData) => {
+
+  // };
+
   return (
     <div id="App">
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        <ChatLog entries={chatMessages} />
+        <ChatLog entries={chatMessages} onUpdateLikedMessage={updateLikedMessage}/>
       </main>
     </div>
   );
