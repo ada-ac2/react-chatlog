73a2ee3a6a606ee107a6b431dfb1ff2d0ccc48f3
larissa <larza.ault@gmail.com>
2023-02-06 17:29:22 -0800

    Added state to track chatEntries, function to update state, and number of likes tracker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog';
+import { useState } from 'react';
 
 const App = () => {
+  // create chatEntries state with imported chatMessages data
+  const [chatEntries, setChatEntries] = useState(chatMessages);
+
+  // Function to update chatEntries
+  const updateEntries = (updatedEntry) => {
+    const entries = chatEntries.map((entry) => {
+      if (entry.id === updatedEntry.id) {
+        return updatedEntry;
+      } else {
+        return entry;
+      }
+    });
+    setChatEntries(entries);
+  };
+
+  // Track total number of likes in conversation
+  let numLikes = 0;
+  for (const entry of chatEntries) {
+    if (entry.liked) {
+      numLikes++;
+    }
+  }
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>{numLikes} ❤️s</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog entries={chatEntries} updateEntries={updateEntries}></ChatLog>
       </main>
     </div>
   );
